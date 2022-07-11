bf2f59ff686ae4cb7f536a8d67cc12fa8327e357
Baidoo Nana Lartey <baidoolartey@gmail.com>
2022-07-11 08:17:42 +0000

    add

---
+++ components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const hero = () => {
+  return (
+    <div>
+        {/* Overlay */}
+    <div/>
+    <div>
+        <h2>the <h2/>
+        <div/>
+  )
+}
+export default Hero
