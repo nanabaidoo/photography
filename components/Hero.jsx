1e6a43250db32aa7d23f2958df17d96ad922fc16
Baidoo Nana Lartey <baidoolartey@gmail.com>
2022-07-12 11:52:30 +0000

    add102

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,20 @@
 
 import React from 'react'
+// import { useEffect, useState } from 'react';
 
-export default function Hero() {
+// export default function Hero() {
+  const Hero = ({heading, message}) =>{
   return (
     <div className=' flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'/>
       <div className='p-5 text-white z-[2] ml-[-10rem] mt-[10rem]' >
-        <h2 className='text-5xl font-bold'>Heading</h2>
-        <p className='py-5 text-xl '>Messsage</p>
+        <h2 className='text-5xl font-bold'>{heading}</h2>
+        <p className='py-5 text-xl '>{message}</p>
         <button className='px-8 py-2 border'>Book</button>
       </div>
     </div>
   )
 }
 
+export default Hero;
---
+++ components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Link from 'next/link';
+import {AiOutlineMenu, AiOutlineclose} from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div className='max-w-[1240px] mt-auto flex justify-between items-center p-4 text-white'>
+        <link href='/'>
+        <h1>Capture</h1>
+        </link>
+        <ul>
+            <li>
+                <link href='/'>Home</link>
+            </li>
+            <li>
+                <link href='/#galley'>Gallary</link>
+            </li>
+            <li>
+                <link href='/portfolio'>Work</link>
+            </li>
+            <li>
+                <link href='/contact'>contact</link>
+            </li>
+        </ul>
+        {/* Mobile version*/}
+        <div>
+            <AiOutlineMenu size={20} />
+        </div>
+        {/* Mobile Menu*/}
+        <div>
+        <ul>
+            <li>
+                <link href='/'>Home</link>
+            </li>
+            <li>
+                <link href='/#galley'>Gallary</link>
+            </li>
+            <li>
+                <link href='/portfolio'>Work</link>
+            </li>
+            <li>
+                <link href='/contact'>contact</link>
+            </li>
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
