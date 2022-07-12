901e25fd5951ae30643af897c0ece6f7f7d06e22
Baidoo Nana Lartey <baidoolartey@gmail.com>
2022-07-12 10:33:56 +0000

    Working

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,17 @@
+
 import React from 'react'
 
-export const hero = () => {
+export default function Hero() {
   return (
-    <div>
-        <h2>hero
-          the boy i going 
-        </h2>
-        </div>
+    <div className=' flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+      {/* Overlay */}
+      <div/>
+      <div>
+        <h2>Heading</h2>
+        <p>Messsage</p>
+        <button>Book</button>
+      </div>
+    </div>
   )
 }
+
