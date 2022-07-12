5c8a4af07248079f252966b58b1a5d755d43106e
Baidoo Nana Lartey <baidoolartey@gmail.com>
2022-07-12 08:56:11 +0000

    addition

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 
-const hero = () => {
-  return ( 
-     
-    <div  className='flex items-center justify-center h-screen mb-12 bg-fixed '>
-        {/* Overlay */}
-        <div/>
-    <div/>    
-        <h2>
-            the Government 
+export const hero = () => {
+  return (
+    <div>
+        <h2>hero
         </h2>
-    </div>
-
+        </div>
   )
 }
-export default hero
